4524d5a7dd93dc652c5525266a8f28a8e4f73816
rwillans <robertjwillans@gmail.com>
2021-06-23 16:59:16 +0000

    add_red_flag_dates

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -9,4 +9,11 @@ from cohortextractor import (
 
 referral_codes = codelist_from_csv("codelists/rob_w-specialist-op-referrals-taken-from-nhse-guidance-on-longpost-covid-68f95f8f.csv", system="snomed", column="term")
 
-pc_diag_codes = codelist_from_csv("codelists/opensafely-nice-managing-the-long-term-effects-of-covid-19-64f1ae69 (1).csv", system="snomed", column="term")
+ongoing_covid_code = codelist_from_csv("codelists/ongoing_covid_codelist.csv", system = "snomed", column = "term")
+pc_code = codelist_from_csv("codelists/post_covid_codelist.csv", system = "snomed", column = "term")
+
+ongoing_and_pc_diag_codes = combine_codelists(ongoing_covid_code, pc_code)
+
+hypoxaemia_codes = codelist_from_csv("codelists/oxygen_saturation.csv", system = "snomed", column = "term")
+
+cardiac_pain_code = codelist_from_csv("codelists/cardiac_pain.csv", system = "snomed", column = "term")
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -6,6 +6,7 @@ from cohortextractor import (
     filter_codes_by_category,
     patients,
 )
+
 from codelists import *
 
 study = StudyDefinition(
@@ -16,7 +17,7 @@ study = StudyDefinition(
         "incidence": 0.95,
     },
 
-    pc_diag_date=patients.with_these_clinical_events(pc_diag_codes,
+    pc_diag_date=patients.with_these_clinical_events(ongoing_and_pc_diag_codes,
                                                      find_first_match_in_period = True,
                                                      returning = "date",
                                                      date_format = "YYYY-MM-DD",
@@ -25,36 +26,63 @@ study = StudyDefinition(
                                                     ),
 
     population=patients.satisfying("has_pc AND one_practice AND age_majority > 17", 
-                                    has_pc = patients.with_these_clinical_events(pc_diag_codes, on_or_before = "2021-01-01"),
+                                    has_pc = patients.with_these_clinical_events(ongoing_and_pc_diag_codes, on_or_before = "2021-01-01"),
                                     one_practice = patients.registered_with_one_practice_between("2019-02-01", "2021-06-01"),
                                     age_majority = patients.age_as_of("pc_diag_date")
     ),
     
     age_at_diag=patients.age_as_of(
-        "2021-01-01",
+        "pc_diag_date",
         return_expectations={"int": {"distribution": "population_ages"}}
     ),
 
     prac_id = patients.registered_practice_as_of("pc_diag_date",
                                                 returning="pseudo_id",
                                                 # options for return_expectations
-                                                return_expectations={"category": {"ratios": {c: 1/1000 for c in range(1,1001)}}}
+                                                #return_expectations={"category": {"ratios": {c: 1/1000 for c in range(1,1001)}}}
                                                 #return_expectations={"category": {"ratios": {"prac_1": 0.3, "prac_2": 0.3, "prac_3": 0.4}}}
-                                                #return_expectations={"rate" : "universal", "int" : {"distribution" : "normal", "mean": 1500, "stddev": 50}}
-    )
+                                                return_expectations={"rate" : "universal", "int" : {"distribution" : "normal", "mean": 1500, "stddev": 50}}
+    ),
 
     #commented out until prac_id works
-    # prac_msoa = patients.registered_practice_as_of("pc_diag_date",
-    #                                         returning='msoa',
-    #                                         return_expectations={"rate":"universal",
-    #                                         #  "category": {
-    #                                         #                       "ratios" : {
-    #                                         #                           'msoa1' : 0.5,
-    #                                         #                           'ms0a2' : 0.5,}
-    #                                         #                   },
-    #                                     #},
-    #                                     )#,
+    prac_msoa = patients.registered_practice_as_of("pc_diag_date",
+                                                    returning='msoa',
+                                                    return_expectations={"rate":"universal",
+                                                                        "category": {"ratios" : {'msoa1' : 0.5, 'msoa2' : 0.5}}}
+    ),
 
-    # #diag_og_covid = patients.with_these_clinical_events()
+    diag_ongoing_covid = patients.with_these_clinical_events(ongoing_covid_code,
+                                                            find_first_match_in_period = True,
+                                                            returning = "date",
+                                                            date_format = "YYYY-MM-DD",
+                                                            return_expectations = {"date": {"earliest":"2019-02-01", "latest":"2021-03-01"},
+                                                                                    "rate": "uniform"}
+                                                                                    ),
+
+    diag_post_covid = patients.with_these_clinical_events(pc_code,
+                                                            find_first_match_in_period = True,
+                                                            returning = "date",
+                                                            date_format = "YYYY-MM-DD",
+                                                            return_expectations = {"date": {"earliest":"2019-02-01", "latest":"2021-03-01"},
+                                                                                    "rate": "uniform"}
+                                                                                    ),
+
+    diag_hypoxaemia = patients.with_these_clinical_events(hypoxaemia_codes,
+                                                            find_first_match_in_period = True,
+                                                            returning = "date",
+                                                            date_format = "YYYY-MM-DD",
+                                                            return_expectations = {"date": {"earliest":"2019-02-01", "latest":"2021-03-01"},
+                                                                                    "rate": "uniform"}
+                                                                                    ),
+
+    diag_cardiac_pain = patients.with_these_clinical_events(cardiac_pain_code,
+                                                            find_first_match_in_period = True,
+                                                            returning = "date",
+                                                            date_format = "YYYY-MM-DD",
+                                                            return_expectations = {"date": {"earliest":"2019-02-01", "latest":"2021-03-01"},
+                                                                                    "rate": "uniform"}
+                                                                                    )
+
+
 
 )
