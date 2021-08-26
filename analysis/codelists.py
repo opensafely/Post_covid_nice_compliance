d468a8710e4b9495e532b56ba1df67db9bfaa11c
Robert Willans <robertjwillans@gmail.com>
2021-08-26 12:13:23 +0100

    Updating codelists and transfer to OpenCodelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -15,10 +15,10 @@ ongoing_and_pc_diag_codes = combine_codelists(ongoing_covid_code, pc_code)
 
 # #diagnostics
 diagnostic_bloods = codelist_from_csv("codelists/user-rob_w_diagnostic-blood-test.csv", system = "snomed", column = "term")
-# diagnostic_bp_test = codelist_from_csv("codelists/diagnostic_bp_test.csv", system = "snomed", column = "term")
-# diagnostic_sit_stand = codelist_from_csv("codelists/diagnostic_sit_stand_test.csv", system = "snomed", column = "term")
-# diagnostic_chest_xray = codelist_from_csv("codelists/diagnostic_chest_x_ray.csv", system = "snomed", column = "term")
-# 
+diagnostic_bp_test = codelist_from_csv("codelists/user-rob_w_-lying-and-standing-blood-pressure-readings.csv", system = "snomed", column = "term")
+diagnostic_sit_stand = codelist_from_csv("codelists/user-rob_w-assessment-using-1-min-sit-to-stand-text.csv", system = "snomed", column = "term")
+diagnostic_chest_xray = codelist_from_csv("codelists/user-rob_w-chest-x-ray-code.csv", system = "snomed", column = "term")
+ 
 # #referrals
 # referral_paed = codelist_from_csv("codelists/referral_paed.csv", system = "snomed", column = "term")
 # referral_respiratory = codelist_from_csv("codelists/referral_resp_med.csv", system = "snomed", column = "term")
@@ -32,17 +32,17 @@ diagnostic_bloods = codelist_from_csv("codelists/user-rob_w_diagnostic-blood-tes
 # referral_ent = codelist_from_csv("codelists/referral_ENT.csv", system = "snomed", column = "term")
 # referral_inf_diseases = codelist_from_csv("codelists/referral_inf_diseases.csv", system = "snomed", column = "term")
 # referral_pc_clinic = codelist_from_csv("codelists/referral_pc_clinic.csv", system = "snomed", column = "term")
-# 
-# #pysch variables
-# risk_of_self_harm = codelist_from_csv("codelists/risk_of_self_harm.csv", system = "snomed", column = "term")
-# referral_psych = codelist_from_csv("codelists/referral_psych.csv", system = "snomed", column = "term")
-# referral_psych_iapt = codelist_from_csv("codelists/referral_psych_iapt.csv", system = "snomed", column = "term")
-# mild_anxiety_or_depression = codelist_from_csv("codelists/mild_anxiety_or_depression.csv", system = "snomed", column = "term")
-# 
-# #management
-# self_care_codes = codelist_from_csv("codelists/mgmt_self_care.csv", system = "snomed", column = "term")
-# primary_care_codes = codelist_from_csv("codelists/mgmt_primary_care.csv", system = "snomed", column = "term")
-# community_care_codes = codelist_from_csv("codelists/mgmt_community_care.csv", system = "snomed", column = "term")
+ 
+#pysch variables
+risk_of_self_harm = codelist_from_csv("codelists/user-rob_w-risk_of_self_harm.csv", system = "snomed", column = "term")
+referral_psych = codelist_from_csv("codelists/user-rob_w-referral-to-iapt-programme-mental-health-team.csv", system = "snomed", column = "term")
+referral_psych_iapt = codelist_from_csv("codelists/user-rob_w-referral-to-liason-psychiatry-service-iapt-programme.csv", system = "snomed", column = "term")
+mild_anxiety_or_depression = codelist_from_csv("codelists/user-rob_w-mild_anxiety-or-depression.csv", system = "snomed", column = "term")
+ 
+#management
+self_care_codes = codelist_from_csv("codelists/user-rob_w-self-management-referral-codes-for-covid-recovery.csv", system = "snomed", column = "term")
+primary_care_codes = codelist_from_csv("codelists/user-rob_w-primary-care-management-code.csv", system = "snomed", column = "term")
+community_care_codes = codelist_from_csv("codelists/user-rob_w-management-in-community-care-code.csv", system = "snomed", column = "term")
 
 #advice
 advice_given = codelist_from_csv("codelists/user-rob_w-advice-about-covid-19-codes.csv", system = "snomed", column = "term")
