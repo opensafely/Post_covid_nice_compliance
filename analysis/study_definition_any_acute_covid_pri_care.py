2f9452844e50f74bf5831a676b0670742570214e
Robert Willans <Robert.Willans@nice.org.uk>
2021-10-04 16:37:31 +0100

    hardcode date

--- analysis/study_definition_any_acute_covid_pri_care.py
+++ analysis/study_definition_any_acute_covid_pri_care.py
@@ -25,7 +25,7 @@ study = StudyDefinition(
         has_acute_covid
         """, 
         has_acute_covid = patients.with_these_clinical_events(acute_covid_codes, on_or_after = "2019-02-01"),
-        one_practice = patients.registered_with_one_practice_between("2019-02-01", index_date),
+        one_practice = patients.registered_with_one_practice_between("2019-02-01", "2021-06-01"),
         age_majority = patients.age_as_of("acute_diag_dat")
     ),
     
