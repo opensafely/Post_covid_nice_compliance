07d43408217dc5de16897e9872f9c32807e84e61
Robert-Willans <robert.willans@nice.org.uk>
2022-02-28 14:20:28 +0000

    remove last two nodes from fig 2

--- analysis/descriptive_coding_script.R
+++ analysis/descriptive_coding_script.R
@@ -165,14 +165,14 @@ Fig_2 <- cohort %>%
   mutate("has_diag_og_covid" = factor(case_when(!is.na(diag_ongoing_covid) ~ "Ongoing Covid", TRUE ~ "No Ongoing Covid"), ordered = TRUE),
          "referral_yourcovidrecovery_website_only" = factor(case_when(!is.na(referral_yourcovidrecovery_website_only) ~ "YCR (website only)", TRUE ~ "No Signpost"), ordered = TRUE),
          "has_diag_post_covid" = factor(case_when(!is.na(diag_post_covid) ~ "Post-COVID-19", TRUE ~ "No Post-COVID-19"), ordered = TRUE),
-         "referral_yourcovidrecovery_program"  = factor(case_when(!is.na(referral_yourcovidrecovery_website_program) ~ "YCR (website program)", TRUE ~ "No Referral"), ordered = TRUE),
-         "referral_pc_clinic" = factor(case_when(!is.na(referral_pc_clinic) ~ "Post Covid Clinic", TRUE ~ "No Referral"), ordered = TRUE)
+         #"referral_yourcovidrecovery_program"  = factor(case_when(!is.na(referral_yourcovidrecovery_website_program) ~ "YCR (website program)", TRUE ~ "No Referral"), ordered = TRUE),
+        # "referral_pc_clinic" = factor(case_when(!is.na(referral_pc_clinic) ~ "Post Covid Clinic", TRUE ~ "No Referral"), ordered = TRUE)
          ) %>% 
   group_by(has_diag_og_covid,
            referral_yourcovidrecovery_website_only,
            has_diag_post_covid,
-           referral_yourcovidrecovery_program,
-           referral_pc_clinic
+          # referral_yourcovidrecovery_program,
+         #  referral_pc_clinic
            ) %>% 
   summarise(freq = n()) %>% 
   filter(freq > 6)
@@ -182,16 +182,18 @@ ggplot(as.data.frame(Fig_2), aes(y=freq,
                                     axis1=has_diag_og_covid,
                                     axis2=referral_yourcovidrecovery_website_only,
                                     axis3=has_diag_post_covid,
-                                    axis4=referral_yourcovidrecovery_program,
-                                    axis5=referral_pc_clinic)) +
+                                #    axis4=referral_yourcovidrecovery_program,
+                                 #   axis5=referral_pc_clinic
+                                )) +
   geom_alluvium(aes(fill = referral_yourcovidrecovery_website_only), aes.bind = TRUE) + 
   geom_stratum(width = 1/6, fill = "black", color = "grey") + 
   geom_label(stat = "stratum", aes(label = after_stat(stratum))) + 
   scale_x_discrete(limits = c("Ongoing Symptomatic Diagnosis",
                               "Yourcovidrecovery website",
-                              "Post-COVID-19 Syndrome",
-                              "Yourcovidrecovery program",
-                              "Post-COVID-19 Clinic"),
+                              "Post-COVID-19 Syndrome"
+                              #"Yourcovidrecovery program",
+                              #"Post-COVID-19 Clinic"
+                              ),
                    expand = c(0.05, 0.05)) + 
   scale_y_continuous(limits = c(0, sum(!is.na(cohort$diag_ongoing_covid))), expand = c(0.005, 0.005)) + 
   ggtitle("Fig. 2 - Patient flow from ongoing covid to referral destinations") + 
@@ -234,62 +236,3 @@ ggplot(as.data.frame(Fig_3), aes(y=freq,
 
 ggsave("output/Fig_3.png", width = 10, height = 7, units = "in")
 write_csv(Fig_3, "output/Fig_3_numbers.csv")
-
-## OP table for OG / PC diagnoses - Second paper [should be post-covid diags only or all patients] - comment out for now
-
-# cohort_og_pc <- read_csv(file = "output/input_ongoing_post_covid.csv",
-#                          col_types = cols(patient_id = col_number(),
-#                                           age_group = col_factor(levels = c("0-17","18-24", "25-34", "35-44", "45-54", "55-69", "70-79", "80+")),
-#                                           imd = col_factor(levels = c("1 (Most Deprived)", "2", "3", "4", "5 (Least Deprived)", "Unknown")),
-#                                           region = col_factor(),
-#                                           sex = col_factor(),
-#                                           ethnicity = col_factor(),
-#                                           gp_contact_count = col_integer(),
-#                                           op_count_card = col_integer(), 
-#                                           op_count_rheum = col_integer(),
-#                                           op_count_respiratory = col_integer(), 
-#                                           op_count_pc = col_integer(), 
-#                                           referral_pc_clinic_counts = col_integer(),
-#                                           age_at_diag = col_integer(), 
-#                                           prac_id = col_integer(),
-#                                           prac_msoa = col_character(), 
-#                                           op_count_neuro = col_integer(), 
-#                                           diagnostic_bp_test = col_double(),
-#                                           .default = col_date(format = "%Y-%m-%d")))
-# 
-# generate_freq_tables_ogpc <- function(cohort_df, grouping_var){
-#   
-#   grouping_var_name = names(cohort_df %>% select({{ grouping_var}} ))
-#   
-#   cohort_df %>% 
-#     group_by({{ grouping_var }}) %>% 
-#     summarise(total_patients = n(),
-#               total_gp_contacts = sum(!is.na(gp_contact_count), na.rm = TRUE),
-#               mean_gp_contacts = mean(!is.na(gp_contact_count), na.rm = TRUE),
-#               total_og_diags = sum(!is.na(diag_ongoing_covid), na.rm = TRUE),
-#               total_pc_diags = sum(!is.na(diag_post_covid), na.rm = TRUE),
-#               total_pc_referrals = sum(!is.na(referral_pc_clinic), na.rm = TRUE),
-#               total_pc_op_visits = sum(op_count_pc, na.rm = TRUE),
-#               mean_pc_op_visits = mean(op_count_pc, na.rm = TRUE),
-#               total_cardiology_op_visits = sum(op_count_card, na.rm = TRUE),
-#               mean_cardio_op_visits = mean(op_count_card, na.rm = TRUE),
-#               total_rheum_op_visits = sum(op_count_rheum, na.rm = TRUE),
-#               mean_rheum_op_visits = mean(op_count_rheum, na.rm = TRUE),
-#               total_respiratory_visits = sum(op_count_respiratory, na.rm = TRUE),
-#               mean_rheum_op_visits = mean(op_count_respiratory, na.rm = TRUE),
-#               total_neuro_visits = sum(op_count_neuro, na.rm = TRUE),
-#               mean_neuro_op_visits = mean(op_count_neuro, na.rm = TRUE)
-#     ) %>% 
-#     rename("Group" = {{ grouping_var }}) %>% 
-#     mutate("Demographic" = grouping_var_name) %>%   
-#     select(Demographic,
-#            everything()) %>% 
-#     ungroup()
-# }
-# 
-# freq_table_og_pc <- demo_vars %>% 
-#   map(~generate_freq_tables_ogpc(grouping_var = .data[[.x]],
-#                             cohort_df = cohort_og_pc)) %>%
-#   bind_rows()
-# 
-# write_csv(freq_table_og_pc, "output/freq_table_op.csv")
